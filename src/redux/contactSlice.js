b5f38e0c740a2302ef64a25a4ba7211025f1a71c
Dimitry995 <98602983+Dimitry995@users.noreply.github.com>
2023-03-14 11:25:40 +0200

    rwi

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -1,11 +1,11 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialContacts = {
   contacts: [
-    { id: 'id-1', name: 'Annie Simpson', number: '234-22-69' },
-          { id: 'id-2', name: 'Elize Kline', number: '661-58-90' },
-          { id: 'id-3', name: 'Dimitre Clements', number: '803-67-40' },
-          { id: 'id-4', name: 'Andronik Copeland', number: '90-527-56' },
+    { id: 'id-1', name: 'Annie Simpson', number: '2342269' },
+          { id: 'id-2', name: 'Elize Kline', number: '6615890' },
+          { id: 'id-3', name: 'Dimitre Clements', number: '8036740' },
+          { id: 'id-4', name: 'Andronik Copeland', number: '9052756' },
   ],
 };
 
@@ -13,8 +13,10 @@ export const contactSlice = createSlice({
   name: 'contact',
   initialState: initialContacts,
   reducers: {
-    addContact(state, action) {
-     state.contacts = [...state.contatcs, action.payload];
+    addContact: {
+      reducer(state, action) {
+        state.contacts.push(action.payload);
+      },
     },
 
     removeContact(state, action) {
--- src/utils/notification.js
+++ src/utils/notification.js
@@ -8,8 +8,8 @@ export const patternName =
   "^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$";
 
 export const patternNumber =
-  '+?d{1,4}?[-.s]?(?d{1,3}?)?[-.s]?d{1,4}[-.s]?d{1,4}[-.s]?d{1,9}';
+"^[0-9-]+$";
 
 export const errorName = 'Name may contain only letters, apostrophe and spaces';
 
-export const errorNumber = 'Phone number must be only digits';
+export const errorNumber = "Phone number must be digits and can contain spaces, dashes, parentheses and can start with +";
